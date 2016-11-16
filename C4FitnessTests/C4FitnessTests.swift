54086689a8f378d57d0511ea39a52b6c67f4d331
Mike Vork <mikevork@mikes-lifetouch-macbook-air.local>
2016-11-15 21:25:00 -0600

    Initial Commit

---
+++ C4FitnessTests/C4FitnessTests.swift
@@ -0,0 +1,36 @@
+//
+//  C4FitnessTests.swift
+//  C4FitnessTests
+//
+//  Created by Mike Vork on 11/15/16.
+//  Copyright © 2016 Mike Vork. All rights reserved.
+//
+
+import XCTest
+@testable import C4Fitness
+
+class C4FitnessTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
